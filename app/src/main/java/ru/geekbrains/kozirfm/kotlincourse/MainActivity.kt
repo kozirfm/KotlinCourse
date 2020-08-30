e24d359bc13c45678fd75cb59ac73cd770369b32
Aleksandr Kazirickiy <kozirfm@yandex.ru>
2020-08-30 15:50:32 +0300

    Home Work, Kotlin Course, Lesson 1, add extensions

--- app/src/main/java/ru/geekbrains/kozirfm/kotlincourse/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/kozirfm/kotlincourse/MainActivity.kt
@@ -1,23 +1,18 @@
 package ru.geekbrains.kozirfm.kotlincourse
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val textView = findViewById<TextView>(R.id.textView)
-        val button = findViewById<Button>(R.id.button)
-
         val hello = "Hello"
         val char = '!'
         val text = "$hello ${getWorld()} $char"
 
-        button.setOnClickListener { textView.text = text }
+        this.button.setOnClickListener { this.textView.text = text }
     }
 
     private fun getWorld() = "World"
